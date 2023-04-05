a99625295d6670e5e2eb79c60e01f6114cedbc72
Hamish Harrison <57675214+Noxirus@users.noreply.github.com>
2023-04-05 16:38:13 -0600

    Added backend framework with Express
    Added placeholder references

---
+++ bored-back-end/routers/dtos/create-reminder.ts
@@ -0,0 +1,3 @@
+export default interface CreateReminderDataTransferObject{
+    title: string
+}
---
+++ bored-back-end/routers/reminders.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import CreateReminderDataTransferObject from "./dtos/create-reminder";
+import Reminder from "./models/reminder";
+
+const router = Router();
+const reminders: Reminder[] = [];
+
+router.get('/', (req, res) => {
+    res.json(reminders);
+});
+
+router.post('/', (req, res) => {
+    const {title} = req.body as CreateReminderDataTransferObject;
+    const reminder = new Reminder(title);
+    reminders.push(reminder);
+    res.status(201).json(reminder);
+})
+
+export default router;
